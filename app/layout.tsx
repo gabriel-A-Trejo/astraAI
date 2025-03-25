449d2f34744de266397a9b6fddf87e7cf7d1f2f9
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2025-03-25 18:45:55 -0500

    feat(layout): add Provider component and viewport meta tag
    Introduce the Provider component to wrap the ThemeProvider and add a viewport meta tag for better mobile responsiveness. This ensures consistent behavior across devices and enhances the user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/ThemeProvider";
+import Provider from "@/components/provider/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,16 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <Provider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );
