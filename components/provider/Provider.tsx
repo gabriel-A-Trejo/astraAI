e16f31a836d37d33c34801a1901de5c2080d8dfc
Gabriel A Trejo <GabrielTrejo433@gmail.com>
2025-03-25 19:17:51 -0500

    feat(provider): add Header component to Provider
    The Header component is now rendered within the Provider to ensure it is consistently displayed across the application. This change improves the structure and layout of the UI.

--- components/provider/Provider.tsx
+++ components/provider/Provider.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Header } from "../header/Header";
 
 const Provider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Provider;
